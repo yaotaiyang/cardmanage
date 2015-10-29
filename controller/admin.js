efdf9ba4bce43f5a9c85c1b44fb5e5dec0a138f2
yaoxy <yaoxy@mysoft.com.cn>
2015-10-29 22:40:29 +0800

    删除无用代码

--- controller/admin.js
+++ controller/admin.js
@@ -2,8 +2,6 @@
  * Created by yaotaiyang on 2014/8/4.
  */
 var init=function(req,res){
-    var db=require("../models/db");
-    console.log(req.session.login);
     if(req.session.login){
         db.find(function(docs){
             res.render("admin",{pageData:docs});
