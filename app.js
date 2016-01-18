5976f39b2dee31eb889632e963e0ea3fd073045c
yaoxy <yaoxy@mysoft.com.cn>
2016-01-18 22:34:25 +0800

    提交代码测试

--- app.js
+++ app.js
@@ -5,41 +5,29 @@
  * Time: 下午9:38
  * To change this template use File | Settings | File Templates.
  */
-var domain = require('domain');
-var express = require('express');
+var express = require("express");
 var router=require("./routes");
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser=require("body-parser");
 var app = express();
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart();
-var todos = require('./routes/todos');
-var cloud = require('./cloud');
 var template = require('art-template');
+var AV = require('leanengine');
 
-
-
-// 设置 view 引擎
 template.config('base', '');
 template.config('openTag','{%');
 template.config('closeTag','%}');
 template.config('compress',true);
 template.config('extname', '.html');
-app.use(cookieParser());
+
 app.engine('.html', template.__express);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
-
-
-
-// 加载云代码方法
-app.use(cloud);
+// 加载 cookieSession 以支持 AV.User  的会话状态
+app.use(AV.Cloud);
+app.use(AV.Cloud.CookieSession({ secret: '05XgTktKPMkU', maxAge: 1000*60*60*24, fetchUser: true }));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use('/public', express.static(__dirname + '/public'));
-app.use(cookieParser());
-
 app.use(router);
 module.exports = app;
-
