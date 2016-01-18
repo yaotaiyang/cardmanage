465436724a258d4ec87d24949c4576bda610d26e
yaoxy <yaoxy@mysoft.com.cn>
2016-01-18 22:24:38 +0800

    修改app.js

--- app.js
+++ app.js
@@ -11,13 +11,14 @@ var router=require("./routes");
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var app = express();
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart();
 var todos = require('./routes/todos');
 var cloud = require('./cloud');
 var template = require('art-template');
 
-var app = express();
+
 
 // 设置 view 引擎
 template.config('base', '');
@@ -25,20 +26,17 @@ template.config('openTag','{%');
 template.config('closeTag','%}');
 template.config('compress',true);
 template.config('extname', '.html');
-
+app.use(cookieParser());
 app.engine('.html', template.__express);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
-//app.use(express.static('public'));
-app.use('/public', express.static(__dirname + '/public'));
-// 加载云代码方法
-app.use(cloud);
 
-//app.use(bodyParser.json());
+
+// 加载云代码方法
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-//app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/public', express.static(__dirname + '/public'));
 app.use(cookieParser());
 
 app.use(router);
