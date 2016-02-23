bb8663cd23434ded2c3bbbe14ce1d34bda107d7f
yaoxy <yaoxy@mysoft.com.cn>
2016-02-23 15:06:08 +0800

    评论相关代码修复

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var AV = require('leanengine');
 
 template.config('base', '');
 template.config('openTag','{%');
-template.config('closeTag','%}');
+template.config('closeTag','%}'); 
 //template.config('compress',true);
 template.config('extname', '.html');
 app.engine('.html', template.__express);
